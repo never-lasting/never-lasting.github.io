64b96318da029a4b527af229a7ce716fdbdd73bc
wangjunqiang <13477093270@163.com>
2020-04-29 07:32:00 +0800

    index.html style

--- ztree/js/index.js
+++ ztree/js/index.js
@@ -1,12 +1,13 @@
 var zTreeObj;
 // zTree 的参数配置，深入使用请参考 API 文档（setting 配置详解）
 var setting = {
-
+	treeId : 'tree',
 	view: {
 		showLine: false,
 		showIcon: false,
 		selectedMulti: false,
-		dblClickExpand: false
+		dblClickExpand: false,
+		target: '_self'
 	},
 	callback: {
 		onClick: function(event, treeId, treeNode, clickFlag) {
@@ -22,44 +23,40 @@ $(document).ready(function() {
 	zTreeObj.expandAll(true);
 
 
-	// $('#searchContent').keyup(function() {
-	// 	uodateTree();
-	// });
-	// $('#tagSelect').change(function() {
-	// 	updateTree();
-	// });
+	$('#searchContent').keydown(function(e) {
+		if (e.keyCode == 13) {
+			updateTree();
+		}
+	});
 
-	function updateTree(){
+	function updateTree() {
 		var searchContent = $('#searchContent').val();
-		var tag = $('#tagSelect').val();
-		
-	}
-
-
-
-	function search() {
-		var searchContent = $('#searchContent').val();
-		var tag = $('#tagSelect').val();
-		var allNodes = zTreeObj.getNodes();
-		if (!searchContent && !tag) {
-			zTreeObj.showNodes(allNodes);
+		if(!$.trim(searchContent)){
+			zTreeObj.showNodes(zTreeObj.getNodesByParam('isHidden', true));
 			return;
 		}
-		var searchNodes = zTreeObj.getNodesByFilter(nodeFilter, false);
-		zTreeObj.hideNodes(allNodes);
-		zTreeObj.showNodes(searchNodes);
+		var notMatchNodes = zTreeObj.getNodesByFilter(notMatchFilter);
+		zTreeObj.showNodes(zTreeObj.getNodesByParam('isHidden', true));
+		zTreeObj.hideNodes(notMatchNodes);
 	}
-
-	function nodeFilter(node) {
+	
+	function notMatchFilter(node) {
 		var searchContent = $('#searchContent').val();
-		var tag = $('#tagSelect').val();
-		if (searchContent && tag) {
-			return node.name.toLocaleUpperCase().indexOf(searchContent.toLocaleUpperCase()) > -1 && node.tags ? node.tags.indexOf(tag) > -1 : false;
+		// var tag = $('#tagSelect').val();
+		var children =  node.children;
+		if (children) {
+			for (var i = 0; i < children.length; i++) {
+				var notMatch = notMatchFilter(children[i]);
+				if (!notMatch) {
+					return false;
+				}
+			}
+			return true;
 		}
-		if (searchContent) {
-			return node.name.toUpperCase().indexOf(searchContent.toUpperCase()) > -1;
-		}
-		return node.tags ? node.tags.indexOf(tag) > -1 : false;
-
+		return node.name.toUpperCase().indexOf(searchContent.toUpperCase()) == -1;
 	}
-});
+	
+	
+	
+	
+})	
