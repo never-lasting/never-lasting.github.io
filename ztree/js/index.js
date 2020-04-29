7be32f80608c5683429726c47d60e9d5991505df
wangjunqiang <13477093270@163.com>
2020-04-29 12:33:16 +0800

    index.html

--- ztree/js/index.js
+++ ztree/js/index.js
@@ -1,17 +1,26 @@
 var zTreeObj;
 // zTree 的参数配置，深入使用请参考 API 文档（setting 配置详解）
 var setting = {
-	treeId : 'tree',
+	treeId: 'tree',
 	view: {
 		showLine: false,
 		showIcon: false,
 		selectedMulti: false,
 		dblClickExpand: false,
-		target: '_self'
+		target: '_self',
+		nodeClasses: {
+			add: ['node']
+		}
 	},
 	callback: {
 		onClick: function(event, treeId, treeNode, clickFlag) {
-			zTreeObj.expandNode(treeNode, !treeNode.open, false, true);
+			if (treeNode.isParent) {
+				zTreeObj.expandNode(treeNode, !treeNode.open, false, true);
+			} else {
+				$('#blog-content').attr('src', treeNode.blogUrl);
+			}
+
+
 		}
 	}
 };
@@ -19,9 +28,25 @@ var setting = {
 //zTree 的数据属性，深入使用请参考 API 文档（zTreeNode 节点数据详解）
 
 $(document).ready(function() {
+
+	// fix height
+	$('.tree-div').height($('body').height() - $('section.page-header').innerHeight() - $('.searchbox').outerHeight(true) -
+		15);
+	$('#blog-content').height($('body').height() - $('section.page-header').innerHeight() - 15)
+
 	zTreeObj = $.fn.zTree.init($('#treeDemo'), setting, nodes);
 	zTreeObj.expandAll(true);
 
+
+	// 
+	var $nodes = $('a.node');
+	for (var i = 0; i < $nodes.length; i++) {
+		if (!$($nodes[i]).next().length) {
+			$($nodes[i]).addClass('leaf')
+		} else {
+			$($nodes[i]).css({"font-size":"24px", "font-weight": "580"});
+		}
+	}
 
 	$('#searchContent').keydown(function(e) {
 		if (e.keyCode == 13) {
@@ -31,7 +56,7 @@ $(document).ready(function() {
 
 	function updateTree() {
 		var searchContent = $('#searchContent').val();
-		if(!$.trim(searchContent)){
+		if (!$.trim(searchContent)) {
 			zTreeObj.showNodes(zTreeObj.getNodesByParam('isHidden', true));
 			return;
 		}
@@ -39,11 +64,11 @@ $(document).ready(function() {
 		zTreeObj.showNodes(zTreeObj.getNodesByParam('isHidden', true));
 		zTreeObj.hideNodes(notMatchNodes);
 	}
-	
+
 	function notMatchFilter(node) {
 		var searchContent = $('#searchContent').val();
 		// var tag = $('#tagSelect').val();
-		var children =  node.children;
+		var children = node.children;
 		if (children) {
 			for (var i = 0; i < children.length; i++) {
 				var notMatch = notMatchFilter(children[i]);
@@ -55,8 +80,8 @@ $(document).ready(function() {
 		}
 		return node.name.toUpperCase().indexOf(searchContent.toUpperCase()) == -1;
 	}
-	
-	
-	
-	
-})	
+
+
+
+
+})
--- ztree/js/nodes.js
+++ ztree/js/nodes.js
@@ -2,17 +2,47 @@ var basePath = "../../";
 var nodes = 
 [
 	{name:"algorithm", children:[
-		{name:"Why capacity must be a power of two?" , url:basePath + "algorithm/[HashMap] why capacity must be a power of two", tags:["algorithm", "HashMap"]}
+		{name:"Why capacity must be a power of two?" , blogUrl:basePath + "algorithm/[HashMap] why capacity must be a power of two", tags:["algorithm", "HashMap"]}
 	]},
 	
 	{name:"jdk", children:[
 		{name:"jdbc", children:[
-			{name:"why Class.forName(…)", url:basePath + "jdk/jdbc/why Class.forName", tags:["jdbc"]}
+			{name:"why Class.forName(…)", blogUrl:basePath + "jdk/jdbc/why Class.forName", tags:["jdbc"]}
 		]}
 	]},
 	
 	{name:"spi", children:[
-		{name:"jdk1.6 spi", url:basePath + "jdk/spi/jdk1.6 spi", tags:["spi", "service loader"]},
-		{name:"spi in jdk", url:basePath + "jdk/spi/spi in jdk",tags:["spi"]}
+		{name:"jdk1.6 spi", blogUrl:basePath + "jdk/spi/jdk1.6 spi", tags:["spi", "service loader"]},
+		{name:"spi in jdk", blogUrl:basePath + "jdk/spi/spi in jdk",tags:["spi"]}
+	]},
+	{name:"spi", children:[
+		{name:"jdk1.6 spi", blogUrl:basePath + "jdk/spi/jdk1.6 spi", tags:["spi", "service loader"]},
+		{name:"spi in jdk", blogUrl:basePath + "jdk/spi/spi in jdk",tags:["spi"]}
+	]},
+	{name:"spi", children:[
+		{name:"jdk1.6 spi", blogUrl:basePath + "jdk/spi/jdk1.6 spi", tags:["spi", "service loader"]},
+		{name:"spi in jdk", blogUrl:basePath + "jdk/spi/spi in jdk",tags:["spi"]}
+	]},
+	{name:"spi", children:[
+		{name:"jdk1.6 spi", blogUrl:basePath + "jdk/spi/jdk1.6 spi", tags:["spi", "service loader"]},
+		{name:"spi in jdk", blogUrl:basePath + "jdk/spi/spi in jdk",tags:["spi"]}
+	]},
+	{name:"spi", children:[
+		{name:"jdk1.6 spi", blogUrl:basePath + "jdk/spi/jdk1.6 spi", tags:["spi", "service loader"]},
+		{name:"spi in jdk", blogUrl:basePath + "jdk/spi/spi in jdk",tags:["spi"]}
+	]},
+	{name:"spi", children:[
+		{name:"jdk1.6 spi", blogUrl:basePath + "jdk/spi/jdk1.6 spi", tags:["spi", "service loader"]},
+		{name:"spi in jdk", blogUrl:basePath + "jdk/spi/spi in jdk",tags:["spi"]}
+	]},
+	{name:"spi", children:[
+		{name:"jdk1.6 spi", blogUrl:basePath + "jdk/spi/jdk1.6 spi", tags:["spi", "service loader"]},
+		{name:"spi in jdk", blogUrl:basePath + "jdk/spi/spi in jdk",tags:["spi"]}
+	]},
+	{name:"spi", children:[
+		{name:"jdk1.6 spi", blogUrl:basePath + "jdk/spi/jdk1.6 spi", tags:["spi", "service loader"]},
+		{name:"spi in jdk", blogUrl:basePath + "jdk/spi/spi in jdk",tags:["spi"]}
 	]}
+
+	
 ];
