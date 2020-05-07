83f6e766b7db231f2551850b7cde0dddf95d813c
wangjunqiang <13477093270@163.com>
2020-05-07 21:06:10 +0800

    whats new in JAVA8

--- ztree/js/nodes.js
+++ ztree/js/nodes.js
@@ -6,14 +6,17 @@ var nodes =
 	]},
 	
 	{name:"jdk", children:[
+		{name:"java8", children:[
+			{name:"what's new in JAVA8", blogUrl:basePath + "jdk/java8/what's new in JAVA8", tages:["JAVA8"]},
+			{name:"Lambda Expression", blogUrl:basePath + "jdk/java8/Lambda Expression", tages:["JAVA8", "Lambda"]}
+		]},
 		{name:"jdbc", children:[
 			{name:"why Class.forName(…)", blogUrl:basePath + "jdk/jdbc/why Class.forName", tags:["jdbc"]}
+		]},
+		{name:"spi", children:[
+			{name:"jdk1.6 spi", blogUrl:basePath + "jdk/spi/jdk1.6 spi", tags:["spi", "service loader"]},
+			{name:"spi in jdk", blogUrl:basePath + "jdk/spi/spi in jdk",tags:["spi"]}
 		]}
-	]},
-	
-	{name:"spi", children:[
-		{name:"jdk1.6 spi", blogUrl:basePath + "jdk/spi/jdk1.6 spi", tags:["spi", "service loader"]},
-		{name:"spi in jdk", blogUrl:basePath + "jdk/spi/spi in jdk",tags:["spi"]}
 	]},
 	
 	{name:"spring", children:[
